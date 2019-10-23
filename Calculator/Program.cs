49f6243760b181c5685c9d0c3c16489d87dd08b6
P N <p.newbranch@outlook.com>
2019-10-23 15:06:55 +0200

    Med snurrande loop för val av räknesätt

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,38 +4,73 @@ namespace Calculator
 {
     class Program
     {
+
+        private static void RunAddition()
+        {
+            Console.WriteLine("+");
+        }
+
+        private static void RunSubtraction()
+        {
+            Console.WriteLine("-");
+        }
+
+        private static void RunMultiplication()
+        {
+            Console.WriteLine("*");
+        }
+
+        private static void RunDivision()
+        {
+            Console.WriteLine("/");
+        }
+
+
+
+
+
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
+            int usersLoopChoise; //for the loop
+            int usersFirstNumber;
+            int usersLastNumber;
 
-            int usersChoise;
-            
-            do {
 
+
+
+            do
+            {
                 Console.WriteLine("RÄKNA RÄTT MED KALKYLATORN");
                 Console.WriteLine();
                 Console.WriteLine("Ange");
-
                 Console.WriteLine("1 för att addera");
                 Console.WriteLine("2 för att subtrahera");
                 Console.WriteLine("3 för att multiplicera");
                 Console.WriteLine("4 för att dividera");
+                Console.WriteLine();
                 Console.WriteLine("9 för att avsluta programmet.");
 
-                Console.WriteLine("Ange N om vill avsluta kalkylatorn: ");
-
-                usersChoise = Console.ReadKey();
-
-                switch usersChoise {
-
-                    case: 1
-                          
-                            
-                
+                               
+                //usersChoise = int.Parse(Console.ReadKey().ToString());
+                usersLoopChoise = int.Parse(Console.ReadLine());
+                switch (usersLoopChoise)
+                {
+                    case 9:
+                        break;
+                    case 1:
+                        RunAddition();
+                        break;
+                    case 2:
+                        RunSubtraction();
+                        break;
+                    case 3:
+                        RunMultiplication();
+                        break;
+                    case 4:
+                        RunDivision();
+                        break;
                 }
-
-
-            } while (loopAgain);
+            } while (usersLoopChoise != 9);
 
 
            
