c45befbec175db12c6e7bb7326d52df5482625dd
P N <p.newbranch@outlook.com>
2019-10-23 13:17:01 +0200

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            int usersChoise;
+            
+            do {
+
+                Console.WriteLine("RÄKNA RÄTT MED KALKYLATORN");
+                Console.WriteLine();
+                Console.WriteLine("Ange");
+
+                Console.WriteLine("1 för att addera");
+                Console.WriteLine("2 för att subtrahera");
+                Console.WriteLine("3 för att multiplicera");
+                Console.WriteLine("4 för att dividera");
+                Console.WriteLine("9 för att avsluta programmet.");
+
+                Console.WriteLine("Ange N om vill avsluta kalkylatorn: ");
+
+                usersChoise = Console.ReadKey();
+
+                switch usersChoise {
+
+                    case: 1
+                          
+                            
+                
+                }
+
+
+            } while (loopAgain);
+
+
+           
+      
+        }
+    }
+}
